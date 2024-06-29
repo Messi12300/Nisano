3b5a4ed30fcada2add881db99fe17a5c8435543b
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2024-06-30 01:08:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5397984467').split()]
 sec_channel = environ.get('SEC_AUTH')
-SEC_AUTH = int(auth_channel) if auth_channel and id_pattern.search(sec_channel) else None
+SEC_AUTH = int(sec_channel) if sec_channel and id_pattern.search(sec_channel) else None
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001934462631').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
